fc7a09c7dfe708a8bdb435036283da687dd0009b
Siddesh Bagare <siddeshb@incubxperts.com>
2023-04-07 12:42:31 +0530

    upadting css

--- src/pages/SideBar.js
+++ src/pages/SideBar.js
@@ -1,7 +1,6 @@
 import { Menu } from "antd";
 import { useNavigate } from "react-router-dom";
 import './Home.css';
-import CustomerForm from "./customer/CustomerForm";
 
 const { SubMenu } = Menu;
 
@@ -10,28 +9,11 @@ const SideBar = () => {
 
     return (
         <div className="Dashboard-nav-bar">
-            {/* <Menu "/dashboard/customer" onClick={({ key }) => {
-                if (key === "logout") {
-
-                }
-                else {
-                    navigate(key);
-                }
-            }}
-                items={[
-                    { className: "Dashboard-nav-bar-title", label: "Home", key: "/dashboard", icon: <HomeOutlined className="Dashboard-nav-bar-icon" /> },
-                    { className: "Dashboard-nav-bar-title", label: "Customers", key: "/dashboard/customer", icon: <UserOutlined className="Dashboard-nav-bar-icon" /> },
-                    { className: "Dashboard-nav-bar-title", label: "Locations", key: "/dashboard/location", icon: <UnorderedListOutlined className="Dashboard-nav-bar-icon" /> },
-                    { className: "Dashboard-nav-bar-title", label: "Logout", key: "logout", icon: <PoweroffOutlined className="Dashboard-nav-bar-icon" /> },
-                ]}>
-
-            </Menu> */}
-
-            <Menu                
+            <Menu
                 mode="inline"
                 onClick={({ key }) => {
                     if (key === "logout") {
-    
+
                     }
                     else {
                         navigate(key);
@@ -41,11 +23,18 @@ const SideBar = () => {
                 <Menu.Item className="Dashboard-nav-bar-title" key="/dashboard">Home</Menu.Item>
                 <SubMenu className="Dashboard-nav-bar-title" title="Customer">
                     <Menu.Item key="/dashboard/customer">Get All</Menu.Item>
+                    <Menu.Item key="/dashboard">Get By Id</Menu.Item>
                     <Menu.Item>Create</Menu.Item>
+                    <Menu.Item>Update</Menu.Item>
+                    <Menu.Item>Delete</Menu.Item>
+                    <Menu.Item>Get Locations </Menu.Item>
                 </SubMenu>
                 <SubMenu className="Dashboard-nav-bar-title" title="Location">
                     <Menu.Item key="/dashboard/location">Get All</Menu.Item>
+                    <Menu.Item key="/dashboard">Get By Id</Menu.Item>
                     <Menu.Item>Create</Menu.Item>
+                    <Menu.Item>Update</Menu.Item>
+                    <Menu.Item>Delete</Menu.Item>
                 </SubMenu>
             </Menu>
 
