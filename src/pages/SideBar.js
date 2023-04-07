c38823729071d8823f08e9313ec99fc259576720
Siddesh Bagare <siddeshb@incubxperts.com>
2023-04-07 12:29:14 +0530

    updated nav bar

--- src/pages/SideBar.js
+++ src/pages/SideBar.js
@@ -1,18 +1,18 @@
 import { Menu } from "antd";
-import { HomeOutlined, PoweroffOutlined, UnorderedListOutlined, UserOutlined } from "@ant-design/icons/lib/icons";
 import { useNavigate } from "react-router-dom";
 import './Home.css';
+import CustomerForm from "./customer/CustomerForm";
 
-//const { SubMenu } = Menu;
+const { SubMenu } = Menu;
 
 const SideBar = () => {
     const navigate = useNavigate();
 
     return (
-        <div>
-            <Menu className="Dashboard-nav-bar" onClick={({ key }) => {
+        <div className="Dashboard-nav-bar">
+            {/* <Menu "/dashboard/customer" onClick={({ key }) => {
                 if (key === "logout") {
-                    
+
                 }
                 else {
                     navigate(key);
@@ -25,41 +25,30 @@ const SideBar = () => {
                     { className: "Dashboard-nav-bar-title", label: "Logout", key: "logout", icon: <PoweroffOutlined className="Dashboard-nav-bar-icon" /> },
                 ]}>
 
-            </Menu>
-            <Menu
-                defaultOpenKeys={['1']}
-                defaultSelectedKeys={['1']}
-                style={{ width: 300 }}
+            </Menu> */}
+
+            <Menu                
                 mode="inline"
                 onClick={({ key }) => {
                     if (key === "logout") {
-
+    
                     }
                     else {
                         navigate(key);
                     }
                 }}
             >
-                </Menu>
+                <Menu.Item className="Dashboard-nav-bar-title" key="/dashboard">Home</Menu.Item>
+                <SubMenu className="Dashboard-nav-bar-title" title="Customer">
+                    <Menu.Item key="/dashboard/customer">Get All</Menu.Item>
+                    <Menu.Item>Create</Menu.Item>
+                </SubMenu>
+                <SubMenu className="Dashboard-nav-bar-title" title="Location">
+                    <Menu.Item key="/dashboard/location">Get All</Menu.Item>
+                    <Menu.Item>Create</Menu.Item>
+                </SubMenu>
+            </Menu>
 
-            {/* <Menu.Item key="/dashboard">Option 14</Menu.Item>
-                <SubMenu key="1" title="Settings">
-                    <Menu.Item key="2">Option 1</Menu.Item>
-                    <Menu.Item key="3">Option 2</Menu.Item>
-                    <SubMenu key="4" title="Sub-Menu">
-                        <Menu.Item key="5">Option 3</Menu.Item>
-                        <Menu.Item key="6">Option 4</Menu.Item>
-                    </SubMenu>
-                </SubMenu>
-                <SubMenu key="7" title="Profile">
-                    <Menu.Item key="8">Option 5</Menu.Item>
-                    <Menu.Item key="9">Option 6</Menu.Item>
-                    <Menu.Item key="10">Option 7</Menu.Item>
-                    <Menu.Item key="11">Option 8</Menu.Item>
-                </SubMenu>
-                <SubMenu key="19" title="Demo">
-                </SubMenu> */}
-            
         </div>
 
 
