9ea86310ab1d9129ce67cb65196abce5ae971a07
Siddesh Bagare <siddeshb@incubxperts.com>
2023-04-07 15:25:53 +0530

    removed update, delete and getbyid from list

--- src/pages/SideBar.js
+++ src/pages/SideBar.js
@@ -23,18 +23,12 @@ const SideBar = () => {
                 <Menu.Item className="Dashboard-nav-bar-title" key="/dashboard">Home</Menu.Item>
                 <SubMenu className="Dashboard-nav-bar-title" title="Customer">
                     <Menu.Item key="/dashboard/customer/get">Get All</Menu.Item>
-                    <Menu.Item key="/dashboard">Get By Id</Menu.Item>
                     <Menu.Item key="/dashboard/customer/create">Create</Menu.Item>
-                    <Menu.Item>Update</Menu.Item>
-                    <Menu.Item>Delete</Menu.Item>
                     <Menu.Item>Get Locations </Menu.Item>
                 </SubMenu>
                 <SubMenu className="Dashboard-nav-bar-title" title="Location">
                     <Menu.Item key="/dashboard/location/get">Get All</Menu.Item>
-                    <Menu.Item key="/dashboard">Get By Id</Menu.Item>
                     <Menu.Item key="/dashboard/location/create">Create</Menu.Item>
-                    <Menu.Item>Update</Menu.Item>
-                    <Menu.Item>Delete</Menu.Item>
                 </SubMenu>
             </Menu>
 
