dac2aa4c2f1c9b63ab8d5f64b474f3b92aeb1a9f
Siddesh Bagare <siddeshb@incubxperts.com>
2023-04-10 12:29:16 +0530

    logout functionality working

--- src/pages/auth/Logout.js
+++ src/pages/auth/Logout.js
@@ -1,14 +1,16 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Logout = () => {
 
     const navigate = useNavigate();
-
-    console.log("Logout");
-    
     localStorage.removeItem("userToken");
     console.log("token deleted");
-    navigate("/");
+
+    useEffect(() => {
+        const timerId = setTimeout(() => navigate("/"), 1000);
+        return () => clearTimeout(timerId);
+      }, [navigate]);
 }
 
 export default Logout;
