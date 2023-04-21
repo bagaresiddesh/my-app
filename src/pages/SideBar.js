4c499de72ebaf240b320269d7d62ee5cf1641b2c
Siddesh Bagare <siddeshb@incubxperts.com>
2023-04-21 12:21:41 +0530

    updated menubar

--- src/pages/SideBar.js
+++ src/pages/SideBar.js
@@ -1,32 +1,37 @@
 import { Menu } from "antd";
 import { useNavigate } from "react-router-dom";
 import './Home.css';
-import MemberIcon from '@rsuite/icons/Member';
-import LocationIcon from '@rsuite/icons/Location';
-
-const { SubMenu } = Menu;
 
 const SideBar = () => {
     const navigate = useNavigate();
 
+    const items = [
+        { label: 'Home', key: '/dashboard', className: 'Dashboard-nav-bar-title' },
+        {
+            label: 'Customer', key: '/dashboard/customer', className: 'Dashboard-nav-bar-title',
+            children: [
+                { label: 'List', key: '/dashboard/customer/get' },
+                { label: 'Create', key: '/dashboard/customer/create' },
+            ],
+        },
+        {
+            label: 'Location', key: '/dashboard/location', className: 'Dashboard-nav-bar-title',
+            children: [
+                { label: 'List', key: '/dashboard/location/get' },
+                { label: 'Create', key: '/dashboard/location/create' },
+            ],
+        },
+        { label: 'Logout', key: '../logout', className: 'Dashboard-nav-bar-title' },
+    ]
+
     return (
         <div className="Dashboard-nav-bar">
-            <Menu
+            <Menu items={items}
                 mode="inline"
                 onClick={({ key }) => {
                     navigate(key);
                 }}
             >
-                <Menu.Item className="Dashboard-nav-bar-title" key="/dashboard">Home</Menu.Item>
-                <SubMenu className="Dashboard-nav-bar-title" key="/dashboard/customer" title="Customer" icon={<MemberIcon className='Home-icon' />}>
-                    <Menu.Item key="/dashboard/customer/get">List</Menu.Item>
-                    <Menu.Item key="/dashboard/customer/create">Create</Menu.Item>
-                </SubMenu>
-                <SubMenu className="Dashboard-nav-bar-title" key="/dashboard/location" title="Location" icon={<LocationIcon className='Home-icon' />}>
-                    <Menu.Item key="/dashboard/location/get">List</Menu.Item>
-                    <Menu.Item key="/dashboard/location/create">Create</Menu.Item>
-                </SubMenu>
-                <Menu.Item className="Dashboard-nav-bar-title" key="../logout">Logout</Menu.Item>
             </Menu>
 
         </div>
